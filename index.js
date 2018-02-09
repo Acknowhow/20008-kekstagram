417f2686236b94940af42ab6687e0c23f5a45371
Vadim Korabelnik <vad@oblako9.ru>
2018-02-09 11:08:06 +0300

    Final checks

--- index.js
+++ index.js
@@ -10,34 +10,47 @@ process.on(`exit`, (code) => {
   console.log(`Exit code: ${code}`);
 });
 
-const color = new Color(util.format
+const _default = util.format
 ( `\n%s\n%s\n%s\n`,
   `Привет пользователь!`,
   `Эта программа будет запускать сервер «{{ ${name} }}».`,
   `Автор: Кекс.`
+);
+
+const _help = util.format
+( `\n%s\n%s\n%s\n`,
+  `Доступные команды:`,
+  `--help – печатает этот текст;`,
+  `--version – печатает версию приложения;`
+);
+
+const _error = new Color(util.format
+( `\n%s\n%s\n`,
+  `Неизвестная команда {{ ${args[0]} }}`,
+  `Чтобы прочитать правила использования приложения, наберите "--help"`
 ), `regexp`);
 
 
 if(!args[0]) {
-  console.log(util.inspect(color, {colors: true}));
+  console.log(_default);
   process.exit(0);
 }
 
 else {
   switch (args[0]) {
-    case `--help`:
-      console.log(`Доступные команды:`);
-      console.log(`--help  – печатает этот текст;`);
 
-      console.log(util.format
-      ( `%s\n`,
-        `--version – печатает версию приложения;`
-      ));
+    case `--help`:
+      console.log(_help);
+      process.exit(0);
       break;
 
     case `--version`:
       console.log(`v0.0.1`);
       break;
+
+    default:
+      console.error(util.inspect(_error, {colors: true}));
+      process.exit(1);
 
   }
 }
