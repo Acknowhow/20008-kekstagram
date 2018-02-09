d56a3a079f0742f302953b4ea3739e027a92aeb8
Vadim Korabelnik <vad@oblako9.ru>
2018-02-09 10:17:17 +0300

    Separate color

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const path = require(`path`);
 const util = require(`util`);
+const path = require(`path`);
+
+const Color = require(`./lib/color`);
 
 const name = path.basename(process.cwd());
 const args = process.argv.slice(2);
@@ -8,55 +10,34 @@ process.on(`exit`, (code) => {
   console.log(`Exit code: ${code}`);
 });
 
-class Color {
-  constructor(string, color) {
-    this.string = string;
-    this.color = color;
-  }
-
-  inspect(depth, options) {
-    const newOptions = Object.assign({}, options, {
-      depth: options.depth === null ? null : options.depth - 1,
-
-    });
-
-    const inner = util.inspect(this.string, newOptions);
-    return `${options.stylize(inner, this.color)}`;
-
-  }
-}
-
-const color = new Color(null, `regexp`);
-
-console.log(util.inspect(color, {colors: true}));
-
-console.log(color);
+const color = new Color(util.format
+( `\n%s\n%s\n%s\n`,
+  `Привет пользователь!`,
+  `Эта программа будет запускать сервер «{{ ${name} }}».`,
+  `Автор: Кекс.`
+), `regexp`);
 
 
 if(!args[0]) {
-  console.log(util.format
-  ( `%s\n%s\n%s`,
-    `Привет пользователь!`,
-    `Эта программа будет запускать сервер «{{ ${name} }}».`,
-    `${util.inspect(`e`)}Автор: Кекс.`
-  ));
+  console.log(util.inspect(color, {colors: true}));
   process.exit(0);
 }
 
 else {
   switch (args[0]) {
     case `--help`:
-      console.log(util.format(
-        `Доступные команды:\n`
-
-      ));
-
+      console.log(`Доступные команды:`);
       console.log(`--help  – печатает этот текст;`);
-      console.log(`--version – печатает версию приложения`);
+
+      console.log(util.format
+      ( `%s\n`,
+        `--version – печатает версию приложения;`
+      ));
       break;
 
     case `--version`:
       console.log(`v0.0.1`);
+      break;
 
   }
 }
