26254b82fe354e24190c690d3773a82538d2f5c3
Vadim Korabelnik <vad@oblako9.ru>
2018-02-13 14:47:21 +0300

    Exported execute

--- src/execute.js
+++ src/execute.js
@@ -1,77 +1,82 @@
-const json = require(`../package.json`);
-const util = require(`util`);
-const path = require(`path`);
+const execute = module.exports = {
+  _exec() {
+    const json = require(`../package.json`);
+    const util = require(`util`);
+    const path = require(`path`);
 
-const Color = require(`./../lib/color`);
-const Formatist = require(`./builder/formatist`);
+    const Color = require(`./../lib/color`);
+    const Formatist = require(`./builder/formatist`);
 
-// Commands
-const c_author = require(`./commands/author`);
-const c_default = require(`./commands/default`);
+    // commands
+    const c_author = require(`./commands/author`);
+    const c_default = require(`./commands/default`);
 
-const c_description = require(`./commands/description`);
-const c_error = require(`./commands/error`);
+    const c_description = require(`./commands/description`);
+    const c_error = require(`./commands/error`);
 
-const c_help = require(`./commands/help`);
-const c_version = require(`./commands/help`);
+    const c_help = require(`./commands/help`);
+    const c_version = require(`./commands/help`);
 
-const args = process.argv.slice(2);
-const app = path.basename(process.cwd());
+    const args = process.argv.slice(2);
+    const app = path.basename(process.cwd());
 
-const author_cl = new Color(`Автор: Кекс`, null);
+    const author_cl = new Color(`Автор: Кекс`, null);
 
-process.on(`exit`, (code) => {
-  console.log(`Exit code: ${code}`);
-});
+    process.on(`exit`, (code) => {
+      console.log(`Exit code: ${code}`);
+    });
 
-// default
-if (!args[0]) {
-  console.log(new Formatist(c_default.execute(
+    // default
+    if (!args[0]) {
+      console.log(new Formatist(c_default.execute(
 
-    app, util.inspect(author_cl, {colors: true})),
-    util).output());
-  process.exit(0);
-} else {
-
-  switch (args[0]) {
-
-    case `--description`:
-
-      console.log(new Formatist(c_description.execute(
-        json.description), util).output());
+        app, util.inspect(author_cl, {colors: true})),
+        util).output());
       process.exit(0);
-      break;
+    } else {
 
-    case `--author`:
+      switch (args[0]) {
 
-      console.log(new Formatist(c_author.execute(
-        json.author), util).output());
-      process.exit(0);
-      break;
+        case `--description`:
 
-    case `--help`:
+          console.log(new Formatist(c_description.execute(
+            json.description), util).output());
+          process.exit(0);
+          break;
 
-      console.log(new Formatist(c_help.execute(
-      ), util).output());
-      process.exit(0);
-      break;
+        case `--author`:
 
-    case `--version`:
+          console.log(new Formatist(c_author.execute(
+            json.author), util).output());
+          process.exit(0);
+          break;
 
-      console.log(new Formatist(c_version.execute(
-        json.version), util).output());
-      process.exit(0);
-      break;
+        case `--help`:
 
-    // wrong command
-    default:
-      // first display error message
-      console.error(util.inspect(new Color(new Formatist(
-        c_error.execute(args[0]), util).output(),
-        `regexp`), {colors: true}));
+          console.log(new Formatist(c_help.execute(
+          ), util).output());
+          process.exit(0);
+          break;
 
-      // then display all available commands
-      console.log(new Formatist(c_help.execute(), util).output());
-      process.exit(1);
+        case `--version`:
+
+          console.log(new Formatist(c_version.execute(
+            json.version), util).output());
+          process.exit(0);
+          break;
+
+        // wrong command
+        default:
+          // first display error message
+          console.error(util.inspect(new Color(new Formatist(
+            c_error.execute(args[0]), util).output(),
+            `regexp`), {colors: true}));
+
+          // then display all available commands
+          console.log(new Formatist(c_help.execute(), util).output());
+          process.exit(1);
+      }
+    }
   }
 }
+
