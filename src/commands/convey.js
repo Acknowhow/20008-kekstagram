ade8d045d6f18a6ee1dd0c080200dc90c9dfa078
Vadim Korabelnik <vad@oblako9.ru>
2018-02-16 01:03:28 +0300

    Fixed according to tutor's request

--- src/commands/convey.js
+++ src/commands/convey.js
@@ -1,67 +1,67 @@
-module.exports = {
-  convey() {
-    // concrete
-    const json = require(`./../../package.json`);
-    const util = require(`util`);
+// concrete
+const json = require(`./../../package.json`);
+const util = require(`util`);
 
-    // builder
-    const colorize = require(`./../builder/color`);
-    const Format = require(`./../builder/format`);
+// builder
+const colorize = require(`./../builder/color`);
+const Format = require(`./../builder/format`);
 
-    // commands
-    const def = require(`./list/default`);
-    const author = require(`./list/author`);
+// commands
+const def = require(`./list/default`);
+const author = require(`./list/author`);
 
-    const help = require(`./list/help`);
-    const version = require(`./list/version`);
+const help = require(`./list/help`);
+const version = require(`./list/version`);
 
-    const description = require(`./list/description`);
-    const error = require(`./list/error`);
+const description = require(`./list/description`);
+const error = require(`./list/error`);
 
-    // C stands for `colorized`
-    const appC = colorize.color(json.name, `name`, util);
-    const authorC = colorize.color(json.author, null, util);
+// C stands for `colorized`
+const appC = colorize.color(json.name, `name`, util);
+const authorC = colorize.color(json.author, null, util);
 
-    const [majorC, minorC, patchC] = json.version.split(`.`);
+const [majorC, minorC, patchC] = json.version.split(`.`);
 
-    const versionC = colorize.color(majorC, `regexp`, util) +
-        `.` + colorize.color(minorC, `string`, util) +
-        `.` + colorize.color(patchC, `boolean`, util);
+const versionC = colorize.color(majorC, `regexp`, util) +
+    `.` + colorize.color(minorC, `string`, util) +
+    `.` + colorize.color(patchC, `boolean`, util);
 
-    const descriptionC = colorize.color(json.description, null, util);
+const descriptionC = colorize.color(json.description, null, util);
 
-    // all commands list
-    const helpHelpC = [colorize.color(help.name, `undefined`, util),
-      colorize.color(help.description, `string`, util)];
+// all commands list
+const helpHelpC = [colorize.color(help.name, `undefined`, util),
+  colorize.color(help.description, `string`, util)];
 
-    const helpVersionC = [colorize.color(version.name, `undefined`, util),
-      colorize.color(version.description, `string`, util)];
+const helpVersionC = [colorize.color(version.name, `undefined`, util),
+  colorize.color(version.description, `string`, util)];
 
-    const helpAuthorC = [colorize.color(author.name, `undefined`, util),
-      colorize.color(author.description, `string`, util)];
+const helpAuthorC = [colorize.color(author.name, `undefined`, util),
+  colorize.color(author.description, `string`, util)];
 
-    const helpDescriptionC = [colorize.color(description.name, `undefined`, util),
-      colorize.color(description.description, `string`, util)];
+const helpDescriptionC = [colorize.color(description.name, `undefined`, util),
+  colorize.color(description.description, `string`, util)];
 
-    return {
+exports.convey = () => {
+  return {
 
-      def: new Format(def.execute(
-          appC, authorC), util).format(),
+    def: new Format(def.execute(
+        appC, authorC), util).format(),
 
-      author: new Format(author.execute(
-          authorC), util).format(),
+    author: new Format(author.execute(
+        authorC), util).format(),
 
-      help: new Format(help.execute(helpHelpC, helpVersionC,
-          helpAuthorC, helpDescriptionC), util).format(),
+    help: new Format(help.execute(helpHelpC, helpVersionC,
+        helpAuthorC, helpDescriptionC), util).format(),
 
-      version: new Format(version.execute(
-          versionC), util).format(),
+    version: new Format(version.execute(
+        versionC), util).format(),
 
-      description: new Format(description.execute(
-          descriptionC), util).format(),
+    description: new Format(description.execute(
+        descriptionC), util).format(),
 
-      error: (command) => colorize.color(`${new Format(error.execute(
-          command), util).format()}`, `regexp`, util)
-    };
-  }
+    error: (command) => colorize.color(`${new Format(error.execute(
+        command), util).format()}`, `regexp`, util)
+  };
 };
+
+
--- src/execute.js
+++ src/execute.js
@@ -1,46 +1,44 @@
-module.exports = {
-  exec() {
+const convey = require(`./../src/commands/convey`);
+const args = process.argv.slice(2);
 
-    const convey = require(`./../src/commands/convey`).convey();
-    const args = process.argv.slice(2);
+exports.exec = () => {
 
-    process.on(`exit`, (code) => {
-      console.log(`Exit code: ${code}`);
-    });
+  process.on(`exit`, (code) => {
+    console.log(`Exit code: ${code}`);
+  });
 
-    if (!args[0]) {
-      console.log(convey[`def`]);
+  if (!args[0]) {
+    console.log(convey.convey()[`def`]);
 
-      process.exit(0);
-    } else {
+    process.exit(0);
+  } else {
 
-      switch (args[0]) {
+    switch (args[0]) {
 
-        case `--description`:
-          console.log(convey[`description`]);
-          process.exit(0);
-          break;
+      case `--description`:
+        console.log(convey.convey()[`description`]);
+        process.exit(0);
+        break;
 
-        case `--author`:
-          console.log(convey[`author`]);
-          process.exit(0);
-          break;
+      case `--author`:
+        console.log(convey.convey()[`author`]);
+        process.exit(0);
+        break;
 
-        case `--help`:
-          console.log(convey[`help`]);
-          process.exit(0);
-          break;
+      case `--help`:
+        console.log(convey.convey()[`help`]);
+        process.exit(0);
+        break;
 
-        case `--version`:
-          console.log(convey[`version`]);
-          process.exit(0);
-          break;
+      case `--version`:
+        console.log(convey.convey()[`version`]);
+        process.exit(0);
+        break;
 
-        default:
-          console.error(convey[`error`](args[0]));
-          console.log(convey[`help`]);
-          process.exit(1);
-      }
+      default:
+        console.error(convey.convey()[`error`](args[0]));
+        console.log(convey.convey()[`help`]);
+        process.exit(1);
     }
   }
 };
